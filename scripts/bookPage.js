5bc3423f6dc2521773de1f9339f3f4786fe4495e
Ali-Hadi-Saker <alihady530@gmail.com>
2024-07-14 20:51:52 +0300

    modified booking form

--- scripts/bookPage.js
+++ scripts/bookPage.js
@@ -4,6 +4,10 @@ const taxitBtn = document.getElementById('book-taxi')
 const taxi = document.getElementById('taxi')
 const hotel = document.getElementById('hotel')
 const flight = document.getElementById('flight')
+const planeFrom = document.getElementById('plane-from')
+const planeTo = document.getElementById('plane-to')
+const planeFromTime = document.getElementById('plane-from-time')
+const planeToTime= document.getElementById('plane-to-time')
 
 
 flightBtn.addEventListener('click',()=>{
