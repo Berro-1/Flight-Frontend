fba528dd8bb57d8ea211836d36a0d0e6545c27b3
Ali-Hadi-Saker <alihady530@gmail.com>
2024-07-15 23:09:13 +0300

    displayed flights data in search window

--- scripts/search.js
+++ scripts/search.js
@@ -1,30 +1,47 @@
-const planeFrom = document.getElementById('plane-from')
-const planeTo = document.getElementById('plane-to')
-const search = document.getElementsByClassName('search-button')[0]
+const tableBody = document.querySelector('#flightTable tbody')
 
-const getFlights = async() => {
-    try {
-        const { data } = await axios.post('http://localhost/Flight-Backend/api/flight/getallFlight.php');
-        
-        
-        data.forEach(flight => {
-            const from = document.createElement('option');
-            const to = document.createElement('option');
+
+window.onload = ()=>{
+    const getallFlight = async ()=>{
+        try {
+            const { data } = await axios.post('http://localhost/Flight-Backend/api/flight/getallFlight.php');
             
-
-            from.innerText= flight.departure_airport;
-            to.innerText = flight.arrival_airport;
-
-            planeFrom.appendChild(from);
-            planeTo.appendChild(to);
-        });
-    } catch (error) {
-        console.error('Error fetching hotels:', error);
+            
+            data.forEach(flight => {
+                const row = document.createElement('tr')
+    
+                const fligh_id = document.createElement('td')
+                fligh_id.innerText = flight.flight_number
+                row.append(fligh_id)
+    
+                const flight_dep = document.createElement('td')
+                flight_dep.innerText = flight.departure_airport
+                row.append(flight_dep)
+    
+                const flight_des = document.createElement('td')
+                flight_des.innerText = flight.arrival_airport
+                row.append(flight_des)
+    
+                const flight_dep_time = document.createElement('td')
+                flight_dep_time.innerText = flight.departure_datetime
+                row.append(flight_dep_time)
+    
+                const flight_des_time = document.createElement('td')
+                flight_des_time.innerText = flight.arrival_datetime
+                row.append(flight_des_time)
+    
+                const flight_seats = document.createElement('td')
+                flight_seats.innerText = flight.available_seats
+                row.append(flight_seats)
+    
+                tableBody.append(row)
+    
+            });
+        } catch (error) {
+            console.error('Error fetching hotels:', error);
+        }
+    
     }
+    getallFlight()
 }
 
-getFlights();
-
-search.addEventListener('click', ()=>{
-    window.location.href = 'search-page.html'
-})
