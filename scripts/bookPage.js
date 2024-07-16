95a91e8d24f48c786bef6ae36dd188f6d9b4c255
Ali-Hadi-Saker <alihady530@gmail.com>
2024-07-16 22:27:48 +0300

    displayed flights in book flight form

--- scripts/bookPage.js
+++ scripts/bookPage.js
@@ -26,3 +26,30 @@ taxitBtn.addEventListener('click',()=>{
     taxi.style.display = 'flex';
 })
 
+const getFlights = async() => {
+    try {
+        const { data } = await axios.get('http://localhost/Flight-Backend/api/flight/getallFlight.php')
+        
+        
+        data.forEach(flight => {
+            const from = document.createElement('option')
+            const to = document.createElement('option')
+            const from_time = document.createElement('option')
+            const to_time = document.createElement('option')
+            
+            from.innerText= flight.departure_airport
+            to.innerText = flight.arrival_airport
+            from_time.innerText = flight.departure_datetime
+            to_time.innerText = flight.arrival_datetime
+
+
+            planeFrom.appendChild(from)
+            planeTo.appendChild(to)
+            planeFromTime.appendChild(from_time)
+            planeToTime.appendChild(to_time)
+        });
+    } catch (error) {
+        console.error('Error fetching hotels:', error)
+    }
+}
+getFlights()
