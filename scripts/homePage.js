60c90ddf91c2933d743e5236297cd0e57853f245
Ali-Hadi-Saker <alihady530@gmail.com>
2024-07-17 23:05:17 +0300

    removed duplecated airport from search option

--- scripts/homePage.js
+++ scripts/homePage.js
@@ -6,19 +6,28 @@ const tableBody = document.querySelector('#flightTable tbody')
 const getFlights = async() => {
     try {
         const { data } = await axios.get('http://localhost/Flight-Backend/api/flight/getallFlight.php');
+        const from_arr = []
+        const to_arr = []
         
         
         data.forEach(flight => {
             const from = document.createElement('option');
             const to = document.createElement('option');
             
-            from.id = flight.departure_airport_id;
-            to.id = flight.destination_airport_id;
-            from.innerText= flight.departure_airport;
-            to.innerText = flight.arrival_airport;
+            
+            if(!from_arr.includes(flight.departure_airport)){
+                from.innerText = flight.departure_airport;
+                planeFrom.appendChild(from);
+                from_arr.push(flight.departure_airport)
+            }
 
-            planeFrom.appendChild(from);
-            planeTo.appendChild(to);
+            if(!to_arr.includes(flight.arrival_airport)){
+                to.innerText = flight.arrival_airport;
+                planeTo.appendChild(to);
+                to_arr.push(flight.arrival_airport)
+            }
+            
+            
         });
     } catch (error) {
         console.error('Error fetching hotels:', error);
